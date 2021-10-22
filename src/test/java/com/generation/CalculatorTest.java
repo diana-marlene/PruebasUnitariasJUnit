a66930e9d1dcda5e239d4ca7c0d2e1dae293e34b
Diana Marlene <diana.sanchez9907@gmail.com>
2021-10-22 17:42:31 -0500

    Pruebas Unitarias JUnit

---
+++ src/test/java/com/generation/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.generation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void addTest()
+    {
+        assertEquals( 8, calculator.addNumbers( 5, 3 ) );
+    }
+
+
+    @Test
+    public void subtractTest()
+    {
+        assertEquals( 5, calculator.subtractNumbers( 10, 5 ) );
+    }
+
+
+    @Test
+    public void multiplyTest()
+    {
+        assertEquals( 15, calculator.multiplyNumbers( 5, 3 ) );
+    }
+
+
+    @Test
+    public void divideTest()
+    {
+        assertEquals( 2, calculator.divideNumbers( 10, 5 ) );
+    }
+}
